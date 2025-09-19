c9c7447c8741a2a7881e01c383e1dd8d36a9d3a7
Cassio Cortelini <cassio_cortelini@hotmail.com>
2025-09-19 08:43:11 -0300

    This is my project of LC Estética Automotiva

---
+++ LC/script.js
@@ -0,0 +1,14 @@
+alert("Seja muito bem vindo a LC Estética Automotiva, estamos em desenvolvimento, aguarde novidades! Contato: (54) 996819844");
+
+function olá() {
+    alert("Seja muito bem vindo a LC Estética Automotiva, estamos em desenvolvimento, aguarde novidades! Contato: (54) 996819844");
+}
+
+
+
+
+console.log("Seja muito bem vindo a LC Estética Automotiva");
+console.log("Estamos em desenvolvimento, aguarde novidades!");
+console.log("Contatos: (11) 9 9999-9999 / (11) 9 8888-8888");
+
+
